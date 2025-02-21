96cbdefd9069663f27aa525bb786780b8d534b63
Soliu Adeosun <sawlew@gmail.com>
2025-02-21 18:02:00 +0100

    update

--- src/component/LearnMore.jsx
+++ src/component/LearnMore.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LearnMore = () => {
   return (
     <a href='#' className='flex items-center gap-2'>
-        <p>Laern More</p>
+        <p>Learn More</p>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3">
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
         </svg>
